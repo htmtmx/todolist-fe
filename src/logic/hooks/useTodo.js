a215177a5d5e3ab229ab34f37cfd59487c624ad9
htmtmx <cesar.pineda@daksha.mx>
2024-09-12 17:36:35 -0600

    🚧 Refactor useTodo hook to load todos from backend

--- src/logic/hooks/useTodo.js
+++ src/logic/hooks/useTodo.js
@@ -1,18 +1,29 @@
-import { useEffect, useReducer } from "react";
-import { todoReducer } from "./../../app/todoApp/todoReducer";
+import { useEffect, useReducer, useState } from "react";
+import { getCookieClient } from "../utils/cookies";
+import { todoReducer } from "@/app/todoApp/todoReducer";
+import { getTodos } from "@/services/todo/todo";
 
 export const useTodo = () => {
 
-  const init = () => {
-    return JSON.parse(localStorage.getItem("todos")) || [];
-
-  }
-
-  const [todos, dispatch] = useReducer(todoReducer, [], init);
+  const token = getCookieClient('auth-token');
+  const [todos, dispatch] = useReducer(todoReducer, []);
+  // Función para cargar los todos desde el backend
+  const loadTodos = async () => {
+    try {
+      const response = await getTodos(token);
+      if (response.ok) {
+        dispatch({ type: "[TODO] Load todos", payload: response.todos });
+      } else {
+        console.error("Failed to fetch todos:", response);
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching todos:", error);
+    } 
+  };
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+    loadTodos();
+  }, [token]);
 
   const onNewTodo = (todo) => {
     const action = {
